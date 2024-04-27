2ca26ca742813103eca40a1a0a0f4a1c2b899ac3
Lina Abdelkhalig <lelsammani0@gmail.com>
2024-04-27 23:13:01 +0300

    task 6

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-""" app views """
+"""
+app views
+defines the routes for the API
+"""
 
 
 from flask import Blueprint
@@ -7,4 +10,4 @@ from flask import Blueprint
 # create app_views as instance of blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-from api.v1.views import index
+from api.v1.views import index, states
