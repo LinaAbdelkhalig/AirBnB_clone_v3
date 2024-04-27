c52bf830367c59e2bfe37134f5772d88cf4c0341
Lina Abdelkhalig <lelsammani0@gmail.com>
2024-04-27 22:05:20 +0300

    fixes task 3

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,7 +3,8 @@
 
 
 from flask import Blueprint
-from api.v1.views import index
 
 # create app_views as instance of blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views import index
