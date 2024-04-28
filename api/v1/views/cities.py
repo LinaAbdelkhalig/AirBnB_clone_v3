11348c782ac4d8347594ee2f3eb4571c95e880ef
NafisaKaruri <nafeesahassan016@gmail.com>
2024-04-28 13:36:54 -0700

    fixed create_city

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -10,7 +10,8 @@ from models.city import City
 from models.state import State
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['GET'],
+                 strict_slashes=False)
 def get_cities_bystate(state_id):
     """ gets a list of all cities in a state"""
     state = storage.get(State, state_id)
@@ -42,23 +43,24 @@ def delete_city(city_id):
     abort(404)
 
 
-@app_views.route('/cities', methods=['POST'], strict_slashes=False)
-def create_city():
+@app_views.route('/states/<state_id>/cities', methods=['POST'],
+                 strict_slashes=False)
+def create_city(state_id):
     """creates a city"""
     if request.content_type != 'application/json':
-        abort(404, 'Not a JSON')
+        abort(400, 'Not a JSON')
 
     state = storage.get(State, state_id)
     if not state:
         abort(404)
 
     if not request.get_json():
-        abort(404, 'Not a JSON')
+        abort(400, 'Not a JSON')
 
-    kwarrgs = request.get_json()
+    kwargs = request.get_json()
     if 'name' not in kwargs:
         abort(400, 'Missing name')
-
+    kwargs['state_id'] = state_id
     city = City(**kwargs)
     city.save()
     return jsonify(city.to_dict()), 201
@@ -67,7 +69,6 @@ def create_city():
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def update_city(city_id):
     """updates a state"""
-
     if request.content_type != "application/json":
         abort(400, 'Not a JSON')
     city = storage.get(City, city_id)
@@ -75,7 +76,7 @@ def update_city(city_id):
         if not request.get_json():
             abort(400, 'Not a JSON')
         for attr, val in request.get_json().items():
-            if attr not in ['id','state_id', 'created_at', 'upd                             ated_at']:
+            if attr not in ['id', 'state_id', 'created_at', 'updated_at']:
                 setattr(city, attr, val)
         city.save()
         return jsonify(city.to_dict()), 200
